7370f4bf860c0a48ffe3984fffc0731a2663f89d
Razi <raziyeh.mollaei@gmail.com>
2022-07-07 16:29:34 +1000

    updated

--- js/index.js
+++ js/index.js
@@ -23,8 +23,11 @@ window.addEventListener('DOMContentLoaded', () => {
   const start = document.querySelector('#start');
   start.addEventListener('click', function (e) {
     document.querySelector('#quizBlock').style.display = 'block';
-    start.style.display = 'none';
+    start.style.display = 'none'  
+    
   });
+
+
   // quizArray QUESTIONS & ANSWERS
   // q = QUESTION, o = OPTIONS, a = CORRECT ANSWER
   // Basic ideas from https://code-boxx.com/simple-javascript-quiz/
@@ -40,11 +43,62 @@ window.addEventListener('DOMContentLoaded', () => {
       a: 3,
     },
     {
-      q: 'What is the capital of Australia',
+      q: 'What is the capital of Australia?',
       o: ['Sydney', 'Canberra', 'Melbourne', 'Perth'],
       a: 1,
     },
+
+    {
+      q:  'which country consumes the most chocolate per capita?',
+      o: ['Danmark', 'Switzerland', 'Canada', 'Netherlans'],
+      a: 1,
+    }, 
+
+    {
+      q: 'What is the rarest M&M color?',
+      o: ['Red', 'Blue', 'Brown', 'Yellow'],
+      a: 2,
+    }
+    
+
   ];
+
+  //Add conuntdown //
+ 
+  const startingMinutes = 1;
+  let time = startingMinutes * 60;
+
+  const countdownEl = document.getElementById('countdown');
+
+  setInterval(updateCountdown, 1000);
+
+  function updateCountdown() {
+
+    const minutes = parseInt(time / 60, 10);
+    let seconds = time % 60;
+     if(seconds == 0){
+       
+     }
+    
+
+    seconds = seconds < 1 ? '0' + seconds : seconds;
+
+    countdownEl.innerHTML = `${minutes}: ${seconds}`;
+    time--;
+    if(sec == -1){
+      clearInterval(time);
+      calculateScore();
+      alert(`Times up!!`);
+    }
+
+    
+    
+  }
+
+
+   // End Add conuntdown //
+
+  
 
   // function to Display the quiz questions and answers from the object
   const displayQuiz = () => {
@@ -57,14 +111,16 @@ window.addEventListener('DOMContentLoaded', () => {
                     <li class="list-group-item" id="li_${index}_1"><input type="radio" name="radio${index}" id="radio_${index}_1"> ${quizItem.o[1]}</li>
                     <li class="list-group-item"  id="li_${index}_2"><input type="radio" name="radio${index}" id="radio_${index}_2"> ${quizItem.o[2]}</li>
                     <li class="list-group-item"  id="li_${index}_3"><input type="radio" name="radio${index}" id="radio_${index}_3"> ${quizItem.o[3]}</li>
+                    
                     </ul>
                     <div>&nbsp;</div>`;
-      quizWrap.innerHTML = quizDisplay;
+      quizWrap.innerHTML = quizDisplay ;
     });
+    
   };
 
   // Calculate the score
-  const calculateScore = () => {
+  const calculateScore = (e) => {
     let score = 0;
     quizArray.map((quizItem, index) => {
       for (let i = 0; i < 4; i++) {
@@ -74,17 +130,62 @@ window.addEventListener('DOMContentLoaded', () => {
         liElement = document.querySelector('#' + li);
         radioElement = document.querySelector('#' + r);
 
+        if (radioElent.checked >= 0) {
+          e.preventDefult()
+        }
+
+
+
         if (quizItem.a == i) {
           //change background color of li element here
+          liElement.style.backgroundColor = "#00A600";
+          radioElement.value = 'true';
+          console.log(i)
+
         }
 
         if (radioElement.checked) {
           // code for task 1 goes here
+         console.log(quizItem.a);
+         score++;
+         scoreHTML.innerHTML =`Your current score is ${score} / 5.`;
+         console.log(`Your current score is ${score}.`);
         }
       }
+      
     });
   };
 
   // call the displayQuiz function
+
   displayQuiz();
+
+  // submit & restart btn //
+  const restartBtn = document.getElementById("restart");
+  const submitBtn = document.getElementById("submit");
+  const questionText = document.getElementById("quizWrap")
+
+  submitBtn.addEventListener("click",submit);
+
+  restartBtn.addEventListener("click", restart);
+   const quizesubmit = document.getElementById('quizesubmit');
+  function submit() {
+  submitBtn.classList.add("hide");   
+  quizWrap.innerHTML ="Congratulations on submitting the Quiz!"
+  //quizesubmit.innerText ="Congratulations on submitting the Quiz!"
+  //console.log("Congratulations on submitting the Quiz!");
+  
+ }
+
+
+ displayQuiz();
+
+
+ function restart(){
+  location.reload();
+}
+
+// submit & restart btn //
 });
+
+
