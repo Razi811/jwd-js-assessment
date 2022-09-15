6b92f91f039c55234cb07dd4c7842a1ba0593326
Razi <raziyeh.mollaei@gmail.com>
2022-09-15 13:59:14 +1000

    updated

--- js/index.js
+++ js/index.js
@@ -1,191 +1,137 @@
 /* ***************************
   JWD JavaScript Assessment
-
-  This code is unfinished. You will need to study it to figure out what it does. Then you will need to use this and
-  your own code, to finish the app. 
+  This code is unfinished. You will need to study it to figure out what it does. Then you will need to use this and your own code, to finish the app. 
   
   The tasks you need to do are below.
-
     TASKS TODO:
-      1. Calculate the score as the total of the number of correct answers
-
-      2. Add an Event listener for the submit button, which will display the score and highlight 
-         the correct answers when the button is clicked. Use the code from lines 67 to 86 to help you.
-
-      3. Add 2 more questions to the app (each question must have 4 options).
-
-      4. Reload the page when the reset button is clicked (hint: search window.location)
-
-      5. Add a countdown timer - when the time is up, end the quiz, display the score and highlight the correct answers
+      [DONE] 1. Add 2 more questions to the app (each question must have 4 options). 
+      [DONE] 2. Calculate the score as the total of the number of correct answers
+      [DONE] 3. Add an Event listener for the submit button, which will display the score and highlight the correct answers when the button is clicked. Study the code in the function calculateScore() to help you.
+      [DONE] 4. Reload the page when the reset button is clicked (hint: search window.location)
+      [] 5. Add a countdown timer - when the time is up, end the quiz, display the score and highlight the correct answers
 *************************** */
 
-window.addEventListener('DOMContentLoaded', () => {
-  const start = document.querySelector('#start');
-  start.addEventListener('click', function (e) {
-    document.querySelector('#quizBlock').style.display = 'block';
-    start.style.display = 'none'  
-    
+window.addEventListener("DOMContentLoaded", () => {
+  const start = document.querySelector("#start");
+
+  var downloadTimer;
+
+  start.addEventListener("click", function (e) {
+    document.querySelector("#quizBlock").style.display = "block";
+    start.style.display = "none";
+
+    var timeleft = 60;
+    downloadTimer = setInterval(function countdown(){
+      document.getElementById("time").innerHTML = timeleft + "&nbsp"+"seconds remaining";
+      timeleft -= 1;
+      if(timeleft <= 0){
+      clearInterval(downloadTimer);
+      document.getElementById("time").innerHTML = "Time is up!"
+      calculateScore();
+      }
+    }, 1000);
+
   });
-
-
   // quizArray QUESTIONS & ANSWERS
   // q = QUESTION, o = OPTIONS, a = CORRECT ANSWER
   // Basic ideas from https://code-boxx.com/simple-javascript-quiz/
   const quizArray = [
     {
-      q: 'Which is the third planet from the sun?',
-      o: ['Saturn', 'Earth', 'Pluto', 'Mars'],
+      q: "Which is the third planet from the sun?",
+      o: ["Saturn", "Earth", "Pluto", "Mars"],
       a: 1, // array index 1 - so Earth is the correct answer here
     },
     {
-      q: 'Which is the largest ocean on Earth?',
-      o: ['Atlantic Ocean', 'Indian Ocean', 'Arctic Ocean', 'Pacific Ocean'],
+      q: "Which is the largest ocean on Earth?",
+      o: ["Atlantic Ocean", "Indian Ocean", "Arctic Ocean", "Pacific Ocean"],
       a: 3,
     },
     {
-      q: 'What is the capital of Australia?',
-      o: ['Sydney', 'Canberra', 'Melbourne', 'Perth'],
+      q: "What is the capital of Australia?",
+      o: ["Sydney", "Canberra", "Melbourne", "Perth"],
       a: 1,
     },
-
     {
-      q:  'which country consumes the most chocolate per capita?',
-      o: ['Danmark', 'Switzerland', 'Canada', 'Netherlans'],
+      q: "Which country consumes  the most chocolate per capita?",
+      o: ["Danmark", "Switzerland", "Canada", "Netherland"],
       a: 1,
-    }, 
-
+    },
     {
-      q: 'What is the rarest M&M color?',
-      o: ['Red', 'Blue', 'Brown', 'Yellow'],
-      a: 2,
+      q: "What is the rarest M&M color?",
+      o: ["Red", "Blue", "Yellow", "Brown"],
+      a: 3,
     }
-    
-
   ];
-
-  //Add conuntdown //
- 
-  const startingMinutes = 1;
-  let time = startingMinutes * 60;
-
-  const countdownEl = document.getElementById('countdown');
-
-  setInterval(updateCountdown, 1000);
-
-  function updateCountdown() {
-
-    const minutes = parseInt(time / 60, 10);
-    let seconds = time % 60;
-     if(seconds == 0){
-       
-     }
-    
-
-    seconds = seconds < 1 ? '0' + seconds : seconds;
-
-    countdownEl.innerHTML = `${minutes}: ${seconds}`;
-    time--;
-    if(sec == -1){
-      clearInterval(time);
-      calculateScore();
-      alert(`Times up!!`);
-    }
-
-    
-    
-  }
-
-
-   // End Add conuntdown //
-
-  
 
   // function to Display the quiz questions and answers from the object
   const displayQuiz = () => {
-    const quizWrap = document.querySelector('#quizWrap');
-    let quizDisplay = '';
+    const quizWrap = document.querySelector("#quizWrap");
+    let quizDisplay = "";
     quizArray.map((quizItem, index) => {
       quizDisplay += `<ul class="list-group">
-                   Q - ${quizItem.q}
+                   <h4>Q - ${quizItem.q}</h4>
                     <li class="list-group-item mt-2" id="li_${index}_0"><input type="radio" name="radio${index}" id="radio_${index}_0"> ${quizItem.o[0]}</li>
                     <li class="list-group-item" id="li_${index}_1"><input type="radio" name="radio${index}" id="radio_${index}_1"> ${quizItem.o[1]}</li>
                     <li class="list-group-item"  id="li_${index}_2"><input type="radio" name="radio${index}" id="radio_${index}_2"> ${quizItem.o[2]}</li>
                     <li class="list-group-item"  id="li_${index}_3"><input type="radio" name="radio${index}" id="radio_${index}_3"> ${quizItem.o[3]}</li>
-                    
                     </ul>
                     <div>&nbsp;</div>`;
-      quizWrap.innerHTML = quizDisplay ;
+      quizWrap.innerHTML = quizDisplay;
     });
-    
   };
 
   // Calculate the score
-  const calculateScore = (e) => {
+  const calculateScore = () => {  
     let score = 0;
     quizArray.map((quizItem, index) => {
       for (let i = 0; i < 4; i++) {
         //highlight the li if it is the correct answer
         let li = `li_${index}_${i}`;
         let r = `radio_${index}_${i}`;
-        liElement = document.querySelector('#' + li);
-        radioElement = document.querySelector('#' + r);
-
-        if (radioElent.checked >= 0) {
-          e.preventDefult()
-        }
-
-
+        
+        liElement = document.querySelector("#" + li);
+        radioElement = document.querySelector("#" + r);
 
         if (quizItem.a == i) {
-          //change background color of li element here
-          liElement.style.backgroundColor = "#00A600";
-          radioElement.value = 'true';
-          console.log(i)
+          liElement.style.backgroundColor = 'green';
+          liElement.style.color = 'white';
 
         }
-
-        if (radioElement.checked) {
-          // code for task 1 goes here
-         console.log(quizItem.a);
-         score++;
-         scoreHTML.innerHTML =`Your current score is ${score} / 5.`;
-         console.log(`Your current score is ${score}.`);
-        }
-      }
       
-    });
-  };
+        if(radioElement.checked){
+          if(quizItem.a == i){
+          liElement.style.backgroundColor = 'green';
+          liElement.style.color = 'white';
+          liElement.style.fontWeight = 'bold';
+
+          score++;
+          }else{
+            liElement.style.color = '#c24444';
+            liElement.style.fontWeight = 'bold';
+
+          }
+        }
+        
+        radioElement.disabled = true;
+      }
+    }
+  );
+
+  const calculatedScore = document.querySelector('#calculatedScore');
+  calculatedScore.innerHTML = `You got ${score} out of 5 questions correct`;
+  scroll(0,0);
+  
+};
 
   // call the displayQuiz function
-
   displayQuiz();
 
-  // submit & restart btn //
-  const restartBtn = document.getElementById("restart");
-  const submitBtn = document.getElementById("submit");
-  const questionText = document.getElementById("quizWrap")
+  const submit = document.querySelector("#btnSubmit");
+  submit.addEventListener("click", function (e) {
+    calculateScore();
+    clearInterval(downloadTimer);
+    document.getElementById("time").innerHTML = "Your results:"
 
-  submitBtn.addEventListener("click",submit);
+  })
 
-  restartBtn.addEventListener("click", restart);
-   const quizesubmit = document.getElementById('quizesubmit');
-  function submit() {
-  submitBtn.classList.add("hide");   
-  quizWrap.innerHTML ="Congratulations on submitting the Quiz!"
-  //quizesubmit.innerText ="Congratulations on submitting the Quiz!"
-  //console.log("Congratulations on submitting the Quiz!");
-  
- }
-
-
- displayQuiz();
-
-
- function restart(){
-  location.reload();
-}
-
-// submit & restart btn //
 });
-
-
